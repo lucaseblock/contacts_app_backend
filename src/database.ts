9589a4c201cd398381f3369d12b8c365c1885995
lucaseblock <lucasblock049@gmail.com>
2024-06-25 03:23:16 -0300

    db port

--- src/database.ts
+++ src/database.ts
@@ -3,8 +3,11 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const dbPort = process.env.DB_PORT ? parseInt(process.env.DB_PORT) : undefined;
+
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
+    port: dbPort,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
